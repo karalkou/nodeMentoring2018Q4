8aafe4451399aa151945290c086b5951aa66d7cd
Yury Karalkou <Yury_Karalkou@epam.com>
2018-12-01 19:06:14 +0300

    Add second part of hw1 (see README.md)
    1. Create json module
    2. Create User, Product modules
    3. Import mentioned above modules into app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+import config from './config/index.json';
+import { User, Product } from './models/index';
+
 console.log('Hello Node.js project.');
+
+console.log('config.name: ', config.name);
+
+new User();
+new Product();
