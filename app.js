f5781d14c72aa2d06971c8f18352007d8b3df470
Yury Karalkou <Yury_Karalkou@epam.com>
2018-12-16 14:56:39 +0300

    Update dirwatcher

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import config from './config/index.json';
 import { User, Product } from './models/index';
+import DirWatcher from './modules/dirwatcher';
 
 console.log('Hello Node.js project.');
 
@@ -7,3 +8,5 @@ console.log('config.name: ', config.name);
 
 new User();
 new Product();
+
+new DirWatcher().watch('./data/', 2000);
--- modules/dirwatcher.js
+++ modules/dirwatcher.js
@@ -1,4 +1,11 @@
-import { EventEmitter } from "events";
+import path from 'path';
+import { EventEmitter } from 'events';
+import { readdir, stat, access } from 'fs';
+import { promisify } from 'util';
+
+const readDirPromisified = promisify(readdir);
+const statPromisified = promisify(stat);
+// const accessPromisified = promisify(access);
 
 export default class DirWatcher extends EventEmitter {
     constructor() {
@@ -6,13 +13,124 @@ export default class DirWatcher extends EventEmitter {
         this.path = null;
         this.delay = 0;
         this.files = [];
+        this.checkForChanges = this.checkForChanges.bind(this);
+        this.handleFileStatus = this.handleFileStatus.bind(this);
     }
 
+    /**
+     *
+     * @param path
+     * @param delay
+     */
     watch(path, delay) {
+        console.log('* watch was inited');
         this.path = path;
         this.delay = delay;
-        this.check();
+        setTimeout(this.checkForChanges, this.delay);
     }
 
-    check() {}
+    /**
+     *
+     * @returns {Promise<void>}
+     */
+    async checkForChanges() {
+        console.log('* checkForChanges was inited');
+
+        const fileNames = await readDirPromisified(this.path);
+        console.log('-- fileNames: \n', fileNames);
+        const removedFiles = this.files.filter(
+            file => !fileNames.includes(file.name)
+        );
+        console.log('-- removedFiles: \n', removedFiles);
+
+        removedFiles.forEach(file => this.emit("dirwatcher:deleted", file));
+
+        this.files = await Promise.all(fileNames.map(this.handleFileStatus));
+        console.log('-- this.files: \n', this.files);
+
+
+        // const stats = await statPromisified(`${this.path}SampleCSVFile_2kb.csv`);
+        // console.log('stats: ', stats);
+
+        /*const isExist = await accessPromisified(
+            `${this.path}SampleCSVFile_2kb.csv`,
+            (err) => {
+                if(!err) {
+                    console.log('file exists promisified');
+                    return true;
+                } else {
+                    console.log('err: ', err);
+                    return false;
+                }
+            }
+        );*/
+
+        /*const isExistOld = access(
+            `${this.path}SampleCSVFile_2kb.csv`,
+            (err) => {
+                if(!err) {
+                    console.log('file exists old');
+                    return true;
+                } else {
+                    console.log('err: ', err);
+                    return false;
+                }
+            }
+        );*/
+        // console.log(`${this.path}SampleCSVFile_2kb.csv`);
+        // console.log('is SampleCSVFile_2kb.csv exist: ', isExist);
+        // console.log('is SampleCSVFile_2kb.csv exist: ', isExistOld);
+        // console.log(this.isFileExist(`${this.path}SampleCSVFile_2k.csv`));
+
+    }
+
+    /**
+     *
+     * @param fileName
+     * @returns {Promise<*>}
+     */
+    async handleFileStatus(fileName) {
+        const file = this.files.find(file => file.name === fileName);
+        let stat = {};
+
+        try {
+            stat = await statPromisified(path.join(this.path, fileName));
+        } catch (err) {
+            console.error(err);
+            return file;
+        }
+
+        if (!file) {
+            const newFile = {
+                name: fileName,
+                lastModified: stat.mtimeMs,
+                path: path.join(this.path, fileName),
+            };
+
+            this.emit("dirwatcher:added", newFile);
+
+            return newFile;
+        }
+
+        if (file.lastModified < stat.mtimeMs) {
+            this.emit("dirwatcher:modified", file);
+
+            file.lastModified = stat.mtimeMs;
+        }
+
+        return file;
+    }
+
+    /**
+     *
+     * @param path
+     * @returns {string}
+     */
+    isFileExist(path) {
+        let result = '';
+        access(path, err => {
+            result = !err
+        });
+        return result;
+    }
 }
