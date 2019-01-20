1a53e8983e4ac579362aca25fb0bd9adb8811559
Yury Karalkou <Yury_Karalkou@epam.com>
2019-01-20 19:27:34 +0300

    Start to configure actions

--- utils/streams.js
+++ utils/streams.js
@@ -1,3 +1,30 @@
+const program = require('commander');
+
+program
+    .version('0.1.0')
+    .option('-a, --action <type>', 'action type')
+    .option('-f, --file [path]', 'set the path to file');
+
+program.on('--help', function () {
+    console.log('Would you mind to help me, sir?');
+});
+
+program.parse(process.argv);
+
+console.log('program: ', program);
+console.log('-----------------------------------------');
+
+const {help, action, file} = program;
+
+console.log('help: ', help);
+console.log('action: ', action);
+console.log('file: ', file);
+
+function actionHandler(arg) {
+    console.log('--- actionHandler', arg);
+}
+
+
 function reverse(str) {
     console.log('reverse', str);
 }
