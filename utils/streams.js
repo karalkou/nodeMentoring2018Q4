c380f8a31c156d54dab81c0bd4ea791c2f11a21a
Yury Karalkou <Yury_Karalkou@epam.com>
2019-01-20 19:14:00 +0300

    Place simple mandatory functions in streams.js

---
+++ utils/streams.js
@@ -0,0 +1,19 @@
+function reverse(str) {
+    console.log('reverse', str);
+}
+
+function transform(str) {
+    console.log('transform', str);
+}
+
+function outputFile(filePath) {
+    console.log('outputFile', filePath);
+}
+
+function convertFromFile(filePath) {
+    console.log('convertFromFile', filePath);
+}
+
+function convertToFile(filePath) {
+    console.log('convertToFile', filePath);
+}
