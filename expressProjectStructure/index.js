351c167e19d2e3e7cc76baeedcef6d62a877dc7b
Karalkou <yury.karalkou@gmail.com>
2019-02-09 16:06:22 +0300

    T4, T5: Create express project folders structure

---
+++ expressProjectStructure/index.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+const port = process.env.PORT || 8080;
+
+app.listen(
+    port,
+    () => {
+        console.log(`App is listening on port ​ ${port}​ !`);
+    }
+);
