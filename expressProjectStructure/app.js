46306a0926ca81c8ad15c9fb687e8cb8766f443e
Karalkou <yury.karalkou@gmail.com>
2019-02-09 17:08:36 +0300

    T8: Add stubs for api endpoints

--- expressProjectStructure/app.js
+++ expressProjectStructure/app.js
@@ -1,9 +1,32 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 
-app.get('/', function (req, res) {
+router.get('/', function (req, res) {
     res.send('Hello World')
 });
+
+router.route('/api/products')
+    .get(function (req, res) {
+        res.send('Return ​ ALL ​products')
+    })
+    .post(function (req, res) {
+    res.send('Add ​ NEW​ product and return it')
+});
+
+router.get('/api/:id', function (req, res) {
+    res.send('Return ​ SINGLE​ product')
+});
+
+router.get('/api/:id/reviews', function (req, res) {
+    res.send('Return ​ ALL ​reviews for a single product')
+});
+
+router.get('/api/users', function (req, res) {
+    res.send('Return ​ ALL​ users')
+});
+
+app.use('/', router);
 
 module.exports = app;
