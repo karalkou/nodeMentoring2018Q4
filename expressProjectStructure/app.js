4b8eb42bcfa181fc22caba2d30da71b334c9859b
Yury Karalkou <Yury_Karalkou@epam.com>
2019-02-11 22:17:43 +0300

    T8: Add data-provider. Configure endpoints

--- expressProjectStructure/app.js
+++ expressProjectStructure/app.js
@@ -1,32 +1,65 @@
 const express = require('express');
+const path = require('path');
+const { readFile } = require('fs');
+const { promisify } = require('util');
+const bodyParser = require('body-parser');
+const dataProvider = require('./controllers/data-provider/index.js');
+
+const readFileAsync = promisify(readFile);
 
 const app = express();
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    res.send('Hello World')
+    readFileAsync(path.resolve(__dirname, './index.html'))
+        .then(data => {
+            if (data) {
+                res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' });
+                res.end(data);
+            } else {
+                res.writeHead(404);
+                res.end('404 not found');
+            }
+        })
+        .catch(err => console.error(err));
 });
+
+app.use(bodyParser.urlencoded());
 
 router.route('/api/products')
     .get(function (req, res) {
-        res.send('Return ​ ALL ​products')
+        respond(dataProvider.readProducts(), res);
     })
     .post(function (req, res) {
-        res.send('Add ​ NEW​ product and return it')
+        console.log('-- req.body: ', req.body);
+        respond(dataProvider.addProduct(req.body), res);
     });
 
-router.get('/api/:id', function (req, res) {
-    res.send('Return ​ SINGLE​ product')
+router.get('/api/products/:id', function (req, res) {
+    respond(dataProvider.readProductById(req.params.id), res);
 });
 
-router.get('/api/:id/reviews', function (req, res) {
-    res.send('Return ​ ALL ​reviews for a single product')
+router.get('/api/products/:id/reviews', function (req, res) {
+    respond(dataProvider.findProductReviews(req.params.id), res);
 });
 
 router.get('/api/users', function (req, res) {
-    res.send('Return ​ ALL​ users')
+    respond(dataProvider.readUsers(), res);
 });
 
 app.use('/', router);
 
 module.exports = app;
+
+function respond(data, res) {
+    if (data === 400) {
+        res.writeHead(400);
+        res.end('400 Bad request');
+    } else if (!data) {
+        res.writeHead(404);
+        res.end('404 Not found');
+    } else {
+        res.writeHead(200, { 'Content-Type': 'application/json; charset=utf8' });
+        res.end(JSON.stringify(data));
+    }
+}
---
+++ expressProjectStructure/controllers/data-provider/index.js
@@ -0,0 +1,5 @@
+const DataProvider = require('./data-provider');
+
+const dataProvider = new DataProvider();
+
+module.exports = dataProvider;
