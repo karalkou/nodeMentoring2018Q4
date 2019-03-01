cf953d4618c019386bf65cfd47737f501f835b19
Yury Karalkou <Yury_Karalkou@epam.com>
2019-03-01 08:59:34 +0300

    Add passport-facebook package. Add needed fb stuff

--- expressProjectStructure/config/consts.js
+++ expressProjectStructure/config/consts.js
@@ -2,4 +2,9 @@ const JWT_SECRET = "Do not tell it anyone";
 
 module.exports = {
     JWTSecret: JWT_SECRET,
+    facebookPassport: {
+        "app_id": "1592135727596634",
+        "app_secret": "5adb18d891785f00901c4b948926c232",
+        "callback": "http://localhost:9001/auth/facebook/callback"
+    },
 };
