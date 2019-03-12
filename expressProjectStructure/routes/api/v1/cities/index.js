45ccf9be9a091ad6e2d595d21c6fedbafb3c2060
Yury Karalkou <Yury_Karalkou@epam.com>
2019-03-12 09:26:55 +0300

    Create 'cities' route

---
+++ expressProjectStructure/routes/api/v1/cities/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+// const dataProvider = require('../../../../helpers/data-provider/index.js');
+// const checkToken = require('./../../../../middlewares/checkJWTToken');
+const MongoClient = require('mongodb').MongoClient;
+const { url: mongoUrl } = require('./../../../../config/db');
+const { respond } = require("../../helpers");
+
+// Use connect method to connect to the Server
+MongoClient.connect(mongoUrl, {}, function (err, client) {
+    if (err) {
+        return console.log(err);
+    }
+    // взаимодействие с базой данных
+    client.close();
+});
+
+router.route('/')
+    .get(/*checkToken, */function (req, res) {
+        // respond(dataProvider.readProducts(), res);
+    })
+    .post(/*checkToken, */function (req, res) {
+        console.log('-- req.body: ', req.body);
+        // respond(dataProvider.addProduct(req.body), res);
+    });
+
+router.get('/:id',/* checkToken, */function (req, res) {
+    // respond(dataProvider.readProductById(req.params.id), res);
+});
+
+router.get('/:id/reviews',/* checkToken, */function (req, res) {
+    // respond(dataProvider.findProductReviews(req.params.id), res);
+});
+
+module.exports = router;
