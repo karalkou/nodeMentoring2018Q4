ea3df9b05d1f7c96d35399927f2fcfd430368bc3
Yury Karalkou <Yury_Karalkou@epam.com>
2019-03-16 18:42:04 +0300

    Get data from DB for products

--- expressProjectStructure/routes/api/v1/products/index.js
+++ expressProjectStructure/routes/api/v1/products/index.js
@@ -1,24 +1,62 @@
 const express = require('express');
 const router = express.Router();
-const dataProvider = require('../../../../helpers/data-provider/index.js');
+const ProductModel = require('./../../../../database/models/product');
+const { createNotFindByIdError, createDBError } = require("./../../../../helpers/errorCreators");
 const checkToken = require('./../../../../middlewares/checkJWTToken');
-const { respond } = require("../../helpers");
 
 router.route('/')
     .get(checkToken, function (req, res) {
-        respond(dataProvider.readProducts(), res);
+        ProductModel.find({})
+            .then(products => res.json(products))
+            .catch(() => next(createDBError()));
     })
     .post(checkToken, function (req, res) {
-        console.log('-- req.body: ', req.body);
-        respond(dataProvider.addProduct(req.body), res);
+        const { body: reqBody } = req;
+        const newProduct = {
+            ...reqBody,
+            reviews: reqBody.reviews || 0,
+        };
+
+        new ProductModel(newProduct)
+            .save()
+            .then(product => res.json(product))
+            .catch(() => next(createDBError()));
     });
 
 router.get('/:id', checkToken, function (req, res) {
-    respond(dataProvider.readProductById(req.params.id), res);
+    ProductModel.findById(req.params.id)
+        .then(product => {
+            if (product) {
+                res.json(product);
+            } else {
+                next(createNotFindByIdError('product', req.params.id));
+            }
+        })
+        .catch(() => next(createDBError()));
+});
+
+router.delete('/:id', checkToken, (req, res, next) => {
+    ProductModel.findByIdAndDelete(req.params.id)
+        .then(product => {
+            if (product) {
+                res.json(product);
+            } else {
+                next(createNotFindByIdError('product', req.params.id));
+            }
+        })
+        .catch(() => next(createDBError()));
 });
 
 router.get('/:id/reviews', checkToken, function (req, res) {
-    respond(dataProvider.findProductReviews(req.params.id), res);
+    ProductModel.findById(req.params.id)
+        .then(product => {
+            if (product) {
+                res.json({ reviews: product.reviews });
+            } else {
+                next(createNotFindByIdError('product', req.params.id));
+            }
+        })
+        .catch(() => next(createDBError()));
 });
 
 module.exports = router;
