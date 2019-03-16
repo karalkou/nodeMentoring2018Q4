61b325510270af75961d15ca3005cca61ed7b06f
Yury Karalkou <Yury_Karalkou@epam.com>
2019-03-16 18:56:09 +0300

    Get data from DB for users

--- expressProjectStructure/routes/api/v1/users/index.js
+++ expressProjectStructure/routes/api/v1/users/index.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const dataProvider = require('../../../../helpers/data-provider/index.js');
+const UserModel = require('./../../../../database/models/user');
+const { createNotFindByIdError, createDBError } = require("./../../../../helpers/errorCreators");
 const checkToken = require('./../../../../middlewares/checkJWTToken');
-const { respond } = require("../../helpers");
 
-router.get('/', checkToken, function (req, res) {
-    respond(dataProvider.readUsers(), res);
+router.get('/', checkToken, (req, res) => {
+    UserModel.find({})
+        .then(users => res.json(users))
+        .catch(() => next(createDBError()));
 });
+
+router.post('/', checkToken, (req, res) => {
+    const { body: reqBody } = req;
+    const newUser = {
+        ...reqBody,
+    };
+
+    new UserModel(newUser)
+        .save()
+        .then(user => res.json(user))
+        .catch(() => next(createDBError()));
+});
+
+router.delete('/:id', checkToken, (req, res, next) => {
+    UserModel.findByIdAndDelete(req.params.id)
+        .then(user => {
+            if (user) {
+                res.json(user);
+            } else {
+                next(createNotFindByIdError('user', req.params.id));
+            }
+        })
+        .catch(() => next(createDBError()));
+});
+
 
 module.exports = router;
