feb8fc847994db81e1e79148d98de123dcf3a1c9
Yury Karalkou <Yury_Karalkou@epam.com>
2019-03-17 20:36:39 +0300

    Connect /products to DB

--- expressProjectStructure/routes/api/v1/products/index.js
+++ expressProjectStructure/routes/api/v1/products/index.js
@@ -1,24 +1,65 @@
 const express = require('express');
 const router = express.Router();
-const dataProvider = require('../../../../helpers/data-provider/index.js');
 const checkToken = require('./../../../../middlewares/checkJWTToken');
-const { respond } = require("../../helpers");
+const models = require('./../../../../database_sql/models');
+
+const { Product } = models;
 
 router.route('/')
-    .get(checkToken, function (req, res) {
-        respond(dataProvider.readProducts(), res);
+    .get(/*checkToken, */function (req, res, next) {
+        Product.findAll({})
+            .then(products => res.json(products))
+            .catch(() => next());
     })
-    .post(checkToken, function (req, res) {
+    .post(/*checkToken, */function (req, res, next) {
         console.log('-- req.body: ', req.body);
-        respond(dataProvider.addProduct(req.body), res);
+        const newProduct = {
+            color: req.body.color || null,
+            isFavorite: req.body.isFavorite || false,
+            name: req.body.name || null,
+            reviews: req.body.reviews || 0,
+        };
+
+        Product.create(newProduct)
+            .then(product => res.json(product))
+            .catch(() => next());
     });
 
-router.get('/:id', checkToken, function (req, res) {
-    respond(dataProvider.readProductById(req.params.id), res);
+router.get('/:id', /*checkToken, */function (req, res, next) {
+    Product.findByPk(req.params.id)
+        .then(product => {
+            if (product) {
+                res.json(product);
+            } else {
+                res.status(404).send({
+                    status: 404,
+                    message: `Cannot find product with id: ${req.params.id}`,
+                });
+            }
+        })
+        .catch(() => next());
 });
 
-router.get('/:id/reviews', checkToken, function (req, res) {
-    respond(dataProvider.findProductReviews(req.params.id), res);
+router.get('/:id/reviews', /*checkToken, */function (req, res, next) {
+    Product.findByPk(req.params.id)
+        .then(product => {
+            if (product) {
+                res.json({ reviews: product.reviews });
+            } else {
+                res.status(404).send({
+                    status: 404,
+                    message: `Cannot find product with id: ${req.params.id}`,
+                });
+            }
+        })
+        .catch(() => next());
+});
+
+router.use('*', (req, res) => {
+    res.status(400).json({
+        status: 400,
+        message: 'Something went wrong. Try again, please',
+    });
 });
 
 module.exports = router;
